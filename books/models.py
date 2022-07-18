78507dc282f3768c2b382ec4646d5aa32309a4b2
hamzeNasirii <hamze_nasirii@yahoo.com>
2022-07-18 20:28:03 +0430

    add book list view

--- books/models.py
+++ books/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    content = models.TextField()
+    description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return self.title
 # Create your models here.
